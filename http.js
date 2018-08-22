66b5055716f3dc70e592b0f90b7675787c9672c5
zhangjingya <zhangjingya1@jd.com>
2018-08-23 01:30:32 +0800

    第二种方法

--- http.js
+++ http.js
@@ -21,22 +21,60 @@ let proxy_https = new httpProxy.createProxyServer({
     }
 });
 
-http.createServer(function(req, res) {
-    console.log(req.headers.host)
-    if (req.headers.host === 'localhost') {
-        proxy_web.proxyRequest(req, res);
-        proxy_web.on('error', function(err, req, res) {
-            if (err) console.log(err);
-            res.writeHead(500);
-            res.end('Oops, something went very wrong...');
-        });
-    } else if (req.headers.host === '127.0.0.1') {
-        // PORT = 443;
-        proxy_api.proxyRequest(req, res);
-        proxy_api.on('error', function(err, req, res) {
-            if (err) console.log(err);
-            res.writeHead(500);
-            res.end('Oops, something went very wrong...');
-        });
-    }
-}).listen(PORT);
+// http.createServer(function(req, res) {
+//     console.log(req.headers.host)
+//     if (req.headers.host === 'localhost') {
+//         proxy_web.proxyRequest(req, res);
+//         proxy_web.on('error', function(err, req, res) {
+//             if (err) console.log(err);
+//             res.writeHead(500);
+//             res.end('Oops, something went very wrong...');
+//         });
+//     } else if (req.headers.host === '127.0.0.1') {
+//         // PORT = 443;
+//         proxy_api.proxyRequest(req, res);
+//         proxy_api.on('error', function(err, req, res) {
+//             if (err) console.log(err);
+//             res.writeHead(500);
+//             res.end('Oops, something went very wrong...');
+//         });
+//     }
+// }).listen(PORT);
+
+
+// 新建一个代理 Proxy Server 对象  
+var proxy = httpProxy.createProxyServer({});  
+  
+// 捕获异常  
+proxy.on('error', function (err, req, res) {  
+  res.writeHead(500, {  
+    'Content-Type': 'text/plain'  
+  });  
+  res.end('Something went wrong. And we are reporting a custom error message.');  
+});  
+    
+// 在每次请求中，调用 proxy.web(req, res config) 方法进行请求分发  
+var server = require('http').createServer(function(req, res) {  
+  // 在这里可以自定义你的路由分发  
+  var host = req.headers.host, ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;  
+  console.log("client ip:" + ip + ", host:" + host);  
+    
+  switch(host){  
+    case 'www.111.cn':   
+        proxy.web(req, res, { target: 'http://localhost:80' });  
+    break;  
+    case 'vote.111.cn':  
+        proxy.web(req, res, { target: 'http://localhost:443' });  
+    break;
+    default:  
+        res.writeHead(200, {  
+            'Content-Type': 'text/plain'  
+        });  
+        res.end('Welcome to my server!');  
+  }  
+});  
+  
+console.log("listening on port 80")  
+server.listen(80);
+console.log('PORT')
+
